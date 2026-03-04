fdf996537a078d895b918224872a4f1b88021c1f
Cristian Sandri <Cristiansandri.cs1515@gmail.com>
2026-03-04 19:58:40 -0300

    Adiciona carrossel de projetos com funcionalidade de arrastar e animação suave

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+//Adicionar função de arrastar com mouse e noo mobile, alem de arrumar a animação para ir no smooth
+
+function moverCarrossel(direcao){
+    const carrossel = document.getElementById("carrossel");
+    const card = carrossel.querySelector(".projetos-card");
+
+    const estilo = window.getComputedStyle(carrossel);
+    const gap = parseInt(estilo.gap) || 0;;
+
+    const larguraCard = card.offsetWidth + gap;
+
+    carrossel.scrollBy({
+        left: larguraCard * direcao,
+        behavior: "smooth"
+    });
+}
+
+
 function enviarMensagem(event) {
     event.preventDefault()
 
